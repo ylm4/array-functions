b448c05755e0d639928c7d52220d3956189be23d
Yanira Morales <ylm4@njit.edu>
2016-10-22 12:02:37 -0400

    made an array splice

--- array.php
+++ array.php
@@ -1,5 +1,6 @@
 <?php
 
+
 echo "Array Slice \n";
 $input = array("apple","banana","cherry","date");
 $output = array_slice($input, -1); //returns "date"
@@ -7,4 +8,8 @@ $output = array_slice($input, -3, 1); //returns "cherry"
 print_r(array_slice($input, 1, -1)); // returns "banana","cherry"
 print_r(array_slice($input, 0, -1)); //returns "apple","banana","cherry"
 
+
+echo "Array Splice \n";
+$new_input = array_splice($input, 0,3); //$input is now ("apple","banana","cherry")
+print_r($new_input);
 ?>
